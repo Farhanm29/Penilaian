061423a38d401d8b0357a108a11d8877f5f38bd8
Farhanm29 <Farhanm2900@gmail.com>
2019-11-20 06:36:02 -0800

    apa saja

--- views/Controller/Mahasiswa.js
+++ views/Controller/Mahasiswa.js
@@ -2,6 +2,17 @@
     'use strict'
     angular.module("Mahasiswa", [])
     .controller("MahasiswaController", function($scope,$http){
+        $scope.DatasMahasiswa = [];
+            $http({
+                method: "get",
+                url: "http://localhost/Penilaian/restapi/mahasiswa",
+                header: {
+                    "Content-Type": "application/json"
+                }
+            }).then(function (response) {
+                $scope.DatasPegawai = response.data.result
+            })
+
 
     })
 })(window.angular);
