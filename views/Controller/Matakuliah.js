60d48f77a43065cdd8b72df7f0bde6eb82a31948
Bagusdxc14 <yudhyajha35@gmail.com>
2019-11-25 15:08:28 +0700

    ubah

--- views/Controller/Matakuliah.js
+++ views/Controller/Matakuliah.js
@@ -14,6 +14,61 @@
             }).then(function (response) {
                 $scope.DatasMatakuliah = response.data.result
             })
+            $scope.Simpan = function () {
+                if ($scope.status == "Simpan") {
+                    $http({
+                        method: "POST",
+                        url: "http://localhost/Penilaian/restapi/matakuliah",
+                        data: $scope.input,
+                        header: {
+                            "Content-Type": "application/json"
+                        }
+                    }).then(function (response) {
+                        $scope.DatasMatakuliah.push(angular.copy($scope.input));
+                        alert("Berhasil di Simpan")
+                    }, function (error) {
+                        console.log(error.message);
+                        alert("GAGAL SIMPAN");
+                    })
+                } else {
+                    $http({
+                        method: "put",
+                        url: "http://localhost/Penilaian/restapi/matakuliah",
+                        data: $scope.input,
+                        header: {
+                            "Content-Type": "application/json"
+                        }
+                    }).then(function (response) {
+                        alert("Berhasil di Diubah");
+                    }, function (error) {
+                        alert("Gagal Update");
+                    })
+                }
+
+            }
+            $scope.Hapus = function (item) {
+                $http({
+                    method: "delete",
+                    url: "http://localhost/Penilaian/restapi/matakuliah?kdmk=" + item.kdmk,
+                }).then(function (response) {
+                    var index = $scope.DatasMatakuliah.indexOf(item);
+                    $scope.DatasMatakuliah.splice(index, 1);
+                    alert("Data Berhasil di Hapus");
+                }, function (error) {
+                    alert("Gagal Menghapus");
+                })
+            }
+            $scope.GetData=function(item){
+                $scope.input = item;
+                $scope.status=update;
+
+            }
+           
+            $scope.GetSimpan = function (item) {
+                $scope.input = {};
+                $scope.status = "Simpan";
+            }
+           
     
 
     })
